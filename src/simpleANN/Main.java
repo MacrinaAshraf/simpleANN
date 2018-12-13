35e7833921fd9f8a739e0a979026f463ff4a8227
Ashenn1 <sohasanad@gmail.com>
2018-12-13 16:35:52 +0200

    Added readfile

--- src/simpleANN/Main.java
+++ src/simpleANN/Main.java
@@ -3,9 +3,11 @@ package simpleANN;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		NeuralNetwork network = new NeuralNetwork();
-		network.readFile("SimpleExample");
+		
+		NeuralNetwork n = new NeuralNetwork();
+		
+		n.readFile("SimpleExample.txt");
+
 	}
 
 }
--- src/simpleANN/NeuralNetwork.java
+++ src/simpleANN/NeuralNetwork.java
@@ -1,10 +1,75 @@
 package simpleANN;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NeuralNetwork {
 	
+	 int inputNodesSize , hiddenNodesSize,outputNodesSize;
+	 int trainingSetSize;
+	 List<Float> inputNodes = new ArrayList<Float>();
+	 List<Float> hiddenNodes = new ArrayList<Float>();
+	 List<Float> outputNodes = new ArrayList<Float>();
 	
 	
 	public void readFile(String filePath) {
+		
+		FileReader filereader;
+		BufferedReader in ;
+		
+		try {
+			filereader = new FileReader(filePath);
+			
+			in = new BufferedReader(filereader);
+			String[] tmp;
+			
+			tmp=in.readLine().split("\\s+");
+			
+			inputNodesSize = Integer.parseInt(tmp[0]);
+			hiddenNodesSize = Integer.parseInt(tmp[1]);
+			outputNodesSize = Integer.parseInt(tmp[2]);
+			
+			tmp[0] = in.readLine();
+			
+			trainingSetSize = Integer.parseInt(tmp[0]);
+			
+			for(int i=0;i<trainingSetSize;i++)
+			{
+				
+				tmp = in.readLine().split("\\s+");
+				
+				for(int j=0, x=0 ;j<tmp.length;j++) {
+					
+					if(j < inputNodesSize ) {
+						inputNodes.add(j, Float.parseFloat(tmp[j]));
+					}
+					
+					else {
+						outputNodes.add(x, Float.parseFloat(tmp[j]));
+						x++;
+					}
+				}
+				
+				
+			}
+			
+			
+		}
+	    
+		catch (FileNotFoundException e) {
+			System.out.println("File is not found.");
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			System.out.println("Problem with input/output.");
+			e.printStackTrace();
+		}	
+		
+		
 		
 	}
 
