5e169257a43801f4958237bfdea04153bc49aafb
Macrina <macrina1997@gmail.com>
2018-12-13 16:08:11 +0200

    Sigmoid function added

--- src/simpleANN/NeuralNetwork.java
+++ src/simpleANN/NeuralNetwork.java
@@ -1,14 +1,23 @@
 package simpleANN;
 
+import  java.lang.Math;
+
 public class NeuralNetwork {
 	
 	
 	
 	public void readFile(String filePath) {
-		
 	}
 
-	public void sigmoidFunction() {
+	public double sigmoidFunction(double sigmoidNode){
+		return (1/(1+  java.lang.Math.pow(Math.E,-1*sigmoidNode)));
 		
 	}
+	
+	public double sigmoidDerivative (double output)
+	{
+		return output*(1-output);
+	}
+	
+	
 }
